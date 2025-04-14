a9e0ad176aafc6950fcdc03ab4bedb1b4a01da82
P MANOHARA REDDY <manoharareddyp97@gmail.com>
2025-04-14 12:17:44 +0530

    All the requirements are implemented and finalised other than UI

--- app.py
+++ app.py
@@ -2,6 +2,157 @@ from flask import *
 import pyttsx3
 import mysql.connector as sql
 import datetime,math
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import ssl
+
+smtp_server = "smtp.gmail.com"
+port = 587
+sender_email = "manoharareddyp8@gmail.com"
+password = "bpgo vgji tmkq lgkv"
+
+def send_msg4(server, sender_email, customer_mails):
+    customer_mails = tuple(customer_mails)
+    query = "SELECT * FROM debt_customer_details WHERE email_id IN {}".format(customer_mails)
+    c.execute(query)
+    rows = c.fetchall()
+    plain = rows 
+    text = f"The following customers have not paid in the last 3 months:\n{plain}"
+    c.execute("select email_id from worker_details where job = 'recovery'")
+    rc = c.fetchall()
+    temp_rc = []
+    for i in rc:
+        temp_rc.append(i[0])
+    try:
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Recovery report"
+        message["From"] = sender_email
+        message["To"] = ", ".join(temp_rc)
+        html = """\
+        <html>
+            <body style="background-color: #364585; font-family: Arial, sans-serif;">
+                <div style="text-align: center; margin-top: 20px;">
+                    <img src="https://drive.google.com/uc?id=1rkUgCxEtznORpG6iUfw1FGpuJRXHGgXW&export=download" alt="PM Electronics Logo" style="width: 300px; height: auto; border-radius: 10px;">
+                </div>
+
+                <h1 style="text-align: center; color: darkblue; background-color: lightyellow; padding: 10px; border-radius: 10px;">
+                    Greetings from PM Electronics Pvt. Ltd
+                </h1>
+
+                <h2 style="color: red; text-align: center; background-color: #ffe4e1; padding: 10px; border-radius: 8px;">
+                    Alert!!! Below customers didn't pay from the past 3 Months
+                </h2>
+        
+                <h2 style="color: red; text-align: center; background-color: #ffe4e1; padding: 10px; border-radius: 8px;">
+                    TAKE IMMEDIATE ACTION & RECOVER
+                </h2>
+            </body>
+        </html>
+        """
+        part_plain = MIMEText(text, "plain")
+        part_html = MIMEText(html, "html")
+        message.attach(part_html)
+        message.attach(part_plain)   
+        server.sendmail(sender_email, temp_rc, message.as_string())
+        print("Email sent successfully to:", ", ".join(temp_rc))
+    except Exception as e:
+        print(f"Failed to send email (Recovery AGENCY): {e}")
+
+def send_msg2(server, sender_email, receiver_mails):
+    try:
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Alert!!!! You forgot debt (Testing Application PM ELECTRONICS-2)"
+        message["From"] = sender_email
+        message["To"] = ", ".join(receiver_mails)
+        html = """\
+        <html>
+            <body style="background-color: #364585; font-family: Arial, sans-serif;">
+                <div style="text-align: center; margin-top: 20px;">
+                    <img src="https://drive.google.com/uc?id=1rkUgCxEtznORpG6iUfw1FGpuJRXHGgXW&export=download" alt="PM Electronics Logo" style="width: 300px; height: auto; border-radius: 10px;">
+                </div>
+
+                <h1 style="text-align: center; color: darkblue; background-color: lightyellow; padding: 10px; border-radius: 10px;">
+                    Greetings from PM Electronics Pvt. Ltd
+                </h1>
+
+                <h2 style="color: red; text-align: center; background-color: #ffe4e1; padding: 10px; border-radius: 8px;">
+                    Alert!!! You bought Items and didn't pay from the past 3 Months
+                </h2>
+        
+                <h2 style="color: #ff0000; text-align: center; background-color: #e0ffe0; padding: 10px; border-radius: 8px;">
+                    You debt is automatically increasing at a rate of 36 percent a year.<br>
+                    pay early.... pay less
+                </h2>
+
+                <h2 style="color: darkgreen; text-align: center; background-color: #e0ffe0; padding: 10px; border-radius: 8px;">
+                    Thank you for shopping from us
+                </h2>
+            </body>
+        </html>
+        """
+        message.attach(MIMEText(html, "html"))
+        server.sendmail(sender_email, receiver_mails, message.as_string())
+        print("Email sent successfully to:", ", ".join(receiver_mails))
+    except Exception as e:
+        print(f"Failed to send email (3 MONTHS CASE): {e}")
+    send_msg4(server, sender_email, receiver_mails)
+
+def send_msg3(server, sender_email, receiver_mails):
+    try:
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Alert!!!! You forgot debt (Testing Application PM ELECTRONICS-2)"
+        message["From"] = sender_email
+        message["To"] = ", ".join(receiver_mails)
+        print(receiver_mails)
+        html = """\
+        <html>
+            <body style="background-color: #364585; font-family: Arial, sans-serif;">
+                <div style="text-align: center; margin-top: 20px;">
+                    <img src="https://drive.google.com/uc?id=1rkUgCxEtznORpG6iUfw1FGpuJRXHGgXW&export=download" alt="PM Electronics Logo" style="width: 300px; height: auto; border-radius: 10px;">
+                </div>
+
+                <h1 style="text-align: center; color: darkblue; background-color: lightyellow; padding: 10px; border-radius: 10px;">
+                    Greetings from PM Electronics Pvt. Ltd
+                </h1>
+
+                <h2 style="color: red; text-align: center; background-color: #ffe4e1; padding: 10px; border-radius: 8px;">
+                    Alert!!! You bought Items and didn't pay from the past 1 Month
+                </h2>
+        
+                <h2 style="color: #ff0000; text-align: center; background-color: #e0ffe0; padding: 10px; border-radius: 8px;">
+                    You debt is automatically increasing at a rate of 36 percent a year.<br>
+                    pay early.... pay less
+                </h2>
+
+                <h2 style="color: darkgreen; text-align: center; background-color: #e0ffe0; padding: 10px; border-radius: 8px;">
+                    Thank you for shopping from us
+                </h2>
+            </body>
+        </html>
+
+        """
+        message.attach(MIMEText(html, "html"))
+        server.sendmail(sender_email, receiver_mails, message.as_string())
+        print("Email sent successfully to:", ", ".join(receiver_mails))
+    except Exception as e:
+        print(f"Failed to send email(1 Month case): {e}")
+
+
+def send_msg(server, sender_email):
+    c.execute("select email_id, date from debt_customer_details")
+    rows = c.fetchall()
+    today = datetime.date.today()
+    more_than_1month = []
+    more_than_3months = []
+    for i in rows:
+        if (today-i[1]).days>90:
+            more_than_3months.append(i[0])
+        elif (today-i[1]).days>30:
+            more_than_1month.append(i[0])
+    send_msg2(server, sender_email, more_than_3months)
+    send_msg3(server, sender_email, more_than_1month)
+
 def interest(p,day_took):
     today = datetime.date.today()
     diff = (today-day_took).days
@@ -45,6 +196,22 @@ def worker():
 @app.route('/dashboard/stock')
 def stock():
     return render_template('/stock.html')
+
+@app.route('/dashboard/customer/alerts')
+def alert():
+    try:
+        context = ssl.create_default_context()
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls(context=context)
+            server.login(sender_email, password)
+            send_msg(server, sender_email)
+
+    except smtplib.SMTPAuthenticationError:
+        print("Failed to authenticate. Check your email credentials.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return redirect(url_for('customer'))
 
 @app.route('/dashboard/stock/update_product')
 def update_product():
@@ -155,9 +322,10 @@ def cash2():
     name = request.form['name']
     phone = request.form['phone']
     product = request.form['product']
+    email = request.form['email']
     c.execute("select cash_price from product_details where product_name = '{}'".format(str(product)))
     amount = c.fetchone()[0]
-    c.execute("insert into paid_customer_details values('{}','{}','{}','{}','{}')".format(name,phone,product,amount,datetime.date.today()))
+    c.execute("insert into paid_customer_details values('{}','{}','{}','{}','{}','{}')".format(name,phone,product,amount,datetime.date.today(),email))
     conn.commit()
     c.execute("update stocks set quantity = quantity-1 where product_name = '{}'".format(product))
     conn.commit()
@@ -172,9 +340,10 @@ def debt2():
     name = request.form['name']
     phone = request.form['phone']
     product = request.form['product']
+    email = request.form['email']
     c.execute("select cash_price from product_details where product_name = '{}'".format(str(product)))
     amount = c.fetchone()[0]
-    c.execute("insert into debt_customer_details values('{}','{}','{}','{}','{}')".format(name,phone,product,amount,datetime.date.today()))
+    c.execute("insert into debt_customer_details values('{}','{}','{}','{}','{}','{}')".format(name,phone,product,amount,datetime.date.today(),email))
     conn.commit()
     c.execute("update stocks set quantity = quantity-1 where product_name = '{}'".format(product))
     conn.commit()
@@ -205,10 +374,11 @@ def clear3():
     c.execute("select * from debt_customer_details where phone = '{}'".format(Phone))
     res2 = c.fetchall()
     name = res2[0][0]
+    email = res2[0][5]
     c.execute("delete from debt_customer_details where phone = '{}'".format(Phone))
     conn.commit()
     product = "Clearance"
-    c.execute("insert into paid_customer_details values('{}','{}','{}','{}','{}')".format(name,Phone,product,debt,datetime.date.today()))
+    c.execute("insert into paid_customer_details values('{}','{}','{}','{}','{}','{}')".format(name,Phone,product,debt,datetime.date.today(),email))
     conn.commit()
     return redirect(url_for('customer'))
 
@@ -246,7 +416,7 @@ def update_worker2():
     phone = request.form['phone']
     salary = request.form['salary']
     job = request.form['job']
-    c.execute("update worker_details set age = '{}', phone = '{}', salary = '{}', job = '{}' where name = '{}'".format(age,phone,salary,job,name))
+    c.execute("update worker_details set DOB = '{}', phone = '{}', salary = '{}', job = '{}' where name = '{}'".format(age,phone,salary,job,name))
     conn.commit()
     return redirect(url_for('worker'))
 
